8062d9bdb890fea99cbe66808efb565ab600d217
psalmodia <39930567+psalmodia@users.noreply.github.com>
2021-01-31 23:56:57 -0500

    add wkhtmltopdf build script

---
+++ make-pdf-from-html.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+wkhtmltopdf -s Letter \
+--zoom 1.0 \
+--margin-top 0.75in \
+--margin-bottom 1in \
+--margin-left 0.75in \
+--margin-right 0.75in \
+--footer-left 'Divine Liturgy Service Booklet (Choir)' \
+--footer-right '[page] of [toPage]' \
+--footer-font-name 'Open Sans' \
+--footer-font-size 10 \
+--footer-spacing 10 \
+Choir-Book-Divine-Liturgy-HTFW.html Choir-Book-Divine-Liturgy-HTFW.pdf
